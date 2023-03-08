768cffd13a9b4f86a699e98188508e61e850186e
Kealohi Grondin <jacob.grondin@wsu.edu>
2023-03-07 22:19:13 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# berry_club_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/berry_club_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.berry_club_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
